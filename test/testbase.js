c9424248e7428c08553a8713031794e53ab4ae55
Chat-Wane <brice.nedelec@gmail.com>
2014-02-13 15:43:16 +0100

    convert id base to bigint

--- test/testbase.js
+++ test/testbase.js
@@ -1,5 +1,6 @@
 var expect = require('expect.js');
 var Mocha  = require('mocha');
+var bigInt = require('big-integer');
 
 var Base = require('../lib/base.js');
 var ID = require('../lib/identifier.js');
@@ -32,30 +33,34 @@ describe('base.js', function() {
     describe('getInterval', function(){
 	it('should return an empty interval at lvl 0', function(){
 	    var base = new Base(3);
-	    var id1 = new ID(17,[0,0],[0,0]); // [1,1]
-	    var id2 = new ID(31,[0,0],[0,0]); // [1,15]
+	    var id1 = new ID(bigInt(17),[0,0],[0,0]); // [1,1]
+	    var id2 = new ID(bigInt(31),[0,0],[0,0]); // [1,15]
 	    expect(base.getInterval(id1,id2,0)).to.be.below(0);
 	});
 	
 	it('should return an interval at level 1 of 13', function(){
 	    var base = new Base(3);
-	    var id1 = new ID(17,[0,0],[0,0]); // [1,1]
-	    var id2 = new ID(31,[0,0],[0,0]); // [1,15]
-	    expect(base.getInterval(id1,id2,1)).to.be.eql(13);
+	    var id1 = new ID(bigInt(17),[0,0],[0,0]); // [1,1]
+	    var id2 = new ID(bigInt(31),[0,0],[0,0]); // [1,15]
+	    expect(base.getInterval(id1,id2,1)
+		   .compare(bigInt(13))).to.be.eql(0);
 	});
 
 	it('should return an interval at level 1 of 14', function(){
 	    var base = new Base(3);
-	    var id1 = new ID(1,[0],[0]); // [1]
-	    var id2 = new ID(31,[0,0],[0,0]); // [1,15]
-	    expect(base.getInterval(id1,id2,1)).to.be.eql(14);
+	    var id1 = new ID(bigInt(1),[0],[0]); // [1]
+	    var id2 = new ID(bigInt(31),[0,0],[0,0]); // [1,15]
+	    expect(base.getInterval(id1,id2,1)
+		   .compare(bigInt(14))).to.be.eql(0);
 	});
 
 	it('should return an interval at level 1 of 11', function(){
 	    var base = new Base(3);
-	    var id1 = new ID(20,[0,0],[0,0]); // [1,4] precedes the [1,3]
-	    var id2 = new ID(19,[0,0],[0,0]); // [1,3] 
-	    expect(base.getInterval(id1,id2,1)).to.be.eql(11);
+	    var id1 = new ID(bigInt(20),[0,0],[0,0]);
+	    // [1,4] precedes the [1,3]
+	    var id2 = new ID(bigInt(19),[0,0],[0,0]); // [1,3] 
+	    expect(base.getInterval(id1,id2,1)
+		   .compare(bigInt(11))).to.be.eql(0);
 	});
 
     });
--- test/testidentifier.js
+++ test/testidentifier.js
@@ -1,4 +1,5 @@
 var expect = require('expect.js');
+var bigInt = require('big-integer');
 var Mocha  = require('mocha');
 
 var ID = require('../lib/identifier.js');
@@ -9,8 +10,8 @@ describe('positions.js', function() {
     
     describe('positions', function(){
 	it('is initialized correctly', function(){
-	    var i1 = new ID(0,[0],[0]);
-	    expect(i1._d).to.be.eql(0);
+	    var i1 = new ID(bigInt(0),[0],[0]);
+	    expect(i1._d.equals(bigInt(0))).to.be.ok();
 	    expect(i1._s).to.have.length(1);
 	    expect(i1._c).to.have.length(1);
 	});
@@ -18,50 +19,50 @@ describe('positions.js', function() {
     
     describe('compare', function(){
 	it('compare digit first of lvl 0', function(){
-	    var i1 = new ID(3,[0],[0]); // d:[3]
-	    var i2 = new ID(5,[0],[0]); // d:[5]
+	    var i1 = new ID(bigInt(3),[0],[0]); // d:[3]
+	    var i2 = new ID(bigInt(5),[0],[0]); // d:[5]
 	    expect(i1.compare(i2)).to.be.below(0);
 	    expect(i2.compare(i1)).to.be.above(0);
 	});
 
 	it('compare digit first of lvl 1', function(){
-	    var i1 = new ID(19,[0,1],[0,1]); // d:[1,3]
-	    var i2 = new ID(37,[0,1],[0,1]); // d:[2,5]
+	    var i1 = new ID(bigInt(19),[0,1],[0,1]); // d:[1,3]
+	    var i2 = new ID(bigInt(37),[0,1],[0,1]); // d:[2,5]
 	    expect(i1.compare(i2)).to.be.below(0);
 	    expect(i2.compare(i1)).to.be.above(0);
 	});
 
 	it('compare digit first of lvl 1 but first eql', function(){
-	    var i1 = new ID(19,[0,1],[0,1]); // d:[1,3]
-	    var i2 = new ID(21,[0,1],[0,1]); // d:[1,5]
+	    var i1 = new ID(bigInt(19),[0,1],[0,1]); // d:[1,3]
+	    var i2 = new ID(bigInt(21),[0,1],[0,1]); // d:[1,5]
 	    expect(i1.compare(i2)).to.be.below(0);
 	    expect(i2.compare(i1)).to.be.above(0);
 	});
 
 	it('compare lvl 1 with source different', function(){
-	    var i1 = new ID(19,[0,3],[0,2]); // d:[1.3]
-	    var i2 = new ID(21,[3,3],[0,2]); // d:[1.5]
+	    var i1 = new ID(bigInt(19),[0,3],[0,2]); // d:[1.3]
+	    var i2 = new ID(bigInt(21),[3,3],[0,2]); // d:[1.5]
 	    expect(i1.compare(i2)).to.be.below(0);
 	    expect(i2.compare(i1)).to.be.above(0);
 	});
 
 	it('compare lvl 1 with clock different', function(){
-	    var i1 = new ID(19,[3,3],[0,1]); // d:[1.3]
-	    var i2 = new ID(21,[3,3],[0,2]); // d:[1.5]
+	    var i1 = new ID(bigInt(19),[3,3],[0,1]); // d:[1.3]
+	    var i2 = new ID(bigInt(21),[3,3],[0,2]); // d:[1.5]
 	    expect(i1.compare(i2)).to.be.below(0);
 	    expect(i2.compare(i1)).to.be.above(0);
 	});
 
 	it('compare lvl 0 with lvl 1; same d,s,c; size matters', function(){
-	    var i1 = new ID(1,[3],[0]); // d:[1]
-	    var i2 = new ID(21,[3,3],[0,2]); // d:[1.5]
+	    var i1 = new ID(bigInt(1),[3],[0]); // d:[1]
+	    var i2 = new ID(bigInt(21),[3,3],[0,2]); // d:[1.5]
 	    expect(i1.compare(i2)).to.be.below(0);
 	    expect(i2.compare(i1)).to.be.above(0);
 	});
 
 	it('compare lvl 1 equal identifiers', function(){
-	    var i1 = new ID(21,[3,3],[0,2]); // d:[1.5]
-	    var i2 = new ID(21,[3,3],[0,2]); // d:[1.5]
+	    var i1 = new ID(bigInt(21),[3,3],[0,2]); // d:[1.5]
+	    var i2 = new ID(bigInt(21),[3,3],[0,2]); // d:[1.5]
 	    expect(i1.compare(i2)).to.be.eql(0);
 	    expect(i2.compare(i1)).to.be.eql(0);
 	});
